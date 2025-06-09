163834aa1d6a52ac15f4e7b9eeefb67d8dbf4da1
yuusuke <s25006@std.it-college.ac.jp>
2025-06-09 16:59:33 +0900

    250609

---
+++ turtle4.py
@@ -0,0 +1,8 @@
+from turtle import *
+shape("turtle")
+col = ["orange","limegreen","gold","plum","tomato"]
+for i in range(5):
+    color(col[1])
+    circle(100)
+    left(72)
+done()
---
+++ turtle5.py
@@ -0,0 +1,8 @@
+from turtle import *
+shape("turtle")
+col=["orange","linegreen","gold","plum","tomato"]
+for i in range(99):
+    forward(250)
+    left(130)
+    right(55)
+done()
