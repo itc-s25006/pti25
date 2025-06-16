8b855308099787542edfda49cf655d551cbaa8a7
itc-s25006 <s25006@std.it-college.ac.jp>
2025-06-16 08:02:02 +0000

    250616

---
+++ def2.py
@@ -0,0 +1,7 @@
+def postTaxprice(price):
+    ans =price *1.1
+    return ans
+
+print(postTaxprice(120),"円")
+print(postTaxprice(128))
+print(postTaxprice(980))
---
+++ jugyo.py
@@ -0,0 +1,7 @@
+score=60
+if score >= 80:
+    print("よく頑張ったね")
+    print("この調子だね")
+else:
+    print("もう少し頑張れ")
+
