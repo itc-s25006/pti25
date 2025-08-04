a31197943882d00cd64df7dea363deb3cb6e3859
yuusuke <s25006@std.it-college.ac.jp>
2025-08-04 16:47:55 +0900

    エントリーポイント追加

--- omikuji.py
+++ omikuji.py
@@ -1,3 +1,9 @@
 import random
-kuji = ["大吉","中吉","小吉","凶"]
-print(random.choice(kuji))
+def main():
+
+    kuji = ["大吉","中吉","小吉","凶"]
+    print(random.choice(kuji))
+
+if __name__=="__main__":
+    main()
+
